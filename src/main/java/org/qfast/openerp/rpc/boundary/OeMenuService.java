97ecac0ea295be882270b50afa36118b654be29d
Ahmed El-mawaziny <amawaziny@gmail.com>
2016-04-19 04:47:41 +0200

    javadoc

--- src/main/java/org/qfast/openerp/rpc/boundary/OeMenuService.java
+++ src/main/java/org/qfast/openerp/rpc/boundary/OeMenuService.java
@@ -45,25 +45,56 @@ import static org.qfast.openerp.rpc.entity.OeMenu._PARENT_ID_ID;
  * Service to manage {@link OeMenu}
  *
  * @author Ahmed El-mawaziny
+ * @since 1.0
  */
 public class OeMenuService extends AbstractOeService<OeMenu> {
 
     public static final String name = MENUS.getName();
     private static final long serialVersionUID = -6623029584982281200L;
 
+    /**
+     * Default constructor
+     *
+     * @param executor instance of {@link OeExecutor}
+     * @see OeExecutor
+     */
     public OeMenuService(OeExecutor executor) {
         super(executor, OeMenu.class);
     }
 
+    /**
+     * Use this constructor if you extended {@link OeMenu} and add some modifications
+     *
+     * @param executor instance of {@link OeExecutor}
+     * @param model    new extended class
+     * @param <C>      type of the extended class must be extended form {@link OeMenu}
+     * @see OeMenu
+     */
     public <C extends OeMenu> OeMenuService(OeExecutor executor, Class<C> model) {
         super(executor, model);
     }
 
+    /**
+     * Get Odoo model name
+     *
+     * @return Odoo model name
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * load logged in user menus
+     * <p>
+     * Check if odoo version less than 8 then call {@link JsonMenu#LOAD}
+     * else call {@link Fun#LOAD_MENUS}
+     *
+     * @return User menus in {@link Map}
+     * @throws OeRpcException
+     * @see OeExecutor#execute(String, Map)
+     * @see OeExecutor#execute(String, String)
+     */
     public Map<String, Object> loadMenus() throws OeRpcException {
         if (executor.getOeVersion().getVersionNumber() < 8) {
             Map<String, Object> params = new HashMap<String, Object>();
@@ -99,6 +130,23 @@ public class OeMenuService extends AbstractOeService<OeMenu> {
         return super.find(this, sc, offset, limit, order, context, columns);
     }
 
+    /**
+     * Load user menus in list of {@link OeMenu}
+     * <p>
+     * check if <code>userId</code> same as logged in user id then call {@link #loadOeMenus()}
+     * else get user group by {@link OeGroupService#findByUserId(Long)}
+     * get menus by no group
+     *
+     * @param userId user id to get menus
+     * @return list of {@link OeMenu}
+     * @throws OeRpcException
+     * @see OeMenu
+     * @see #addChildren(List, OeMenu[])
+     * @see #loadOeMenus()
+     * @see OeGroupService
+     * @see #findByGroupId(Long...)
+     * @see #findByNoGroup(String...)
+     */
     public List<OeMenu> findByUserIdAllInOne(Long userId) throws OeRpcException {
         if (OeUtil.equals(userId, executor.getUserId())) {
             List<OeMenu> oeMenus = new ArrayList<OeMenu>(10);
@@ -122,6 +170,12 @@ public class OeMenuService extends AbstractOeService<OeMenu> {
         }
     }
 
+    /**
+     * add Menu's children
+     *
+     * @param menus    list of {@link OeMenu}
+     * @param children array of {@link OeMenu}
+     */
     private void addChildren(List<OeMenu> menus, OeMenu[] children) {
         menus.addAll(Arrays.asList(children));
         for (OeMenu oeMenu : children) {
@@ -129,6 +183,20 @@ public class OeMenuService extends AbstractOeService<OeMenu> {
         }
     }
 
+    /**
+     * load all menus under one menu root as a tree
+     * <p>
+     * Check if <code>userId</code> if same as logged in user
+     * then call {@link #loadOeMenus()}
+     * else find user's group and find menus by group and menus with no group
+     *
+     * @param userId user id to get menus
+     * @return OeMenu root with id -1 and name root and its children linked
+     * @throws OeRpcException
+     * @see OeGroupService
+     * @see OeGroup
+     * @see #setMenuChildren(OeMenu, Long[])
+     */
     public OeMenu findByUserId(Long userId) throws OeRpcException {
         if (!OeUtil.equals(userId, executor.getUserId())) {
             OeGroupService oeGroupService = new OeGroupService(executor);
@@ -150,6 +218,15 @@ public class OeMenuService extends AbstractOeService<OeMenu> {
         }
     }
 
+    /**
+     * set menus children by getting group's menus with parent id and no group with parent id
+     *
+     * @param parentMenu root menu
+     * @param oeGroupsId array of groups id
+     * @throws OeRpcException
+     * @see #findByGroupIdParentId(Long, Long...)
+     * @see OeMenu
+     */
     private void setMenuChildren(OeMenu parentMenu, Long[] oeGroupsId) throws OeRpcException {
         Set<OeMenu> menus = new HashSet<OeMenu>(10);
         menus.addAll(findByGroupIdParentId(parentMenu.getId(), oeGroupsId));
@@ -160,26 +237,65 @@ public class OeMenuService extends AbstractOeService<OeMenu> {
         }
     }
 
-    public Set<OeMenu> findByNoGroup() throws OeRpcException {
+    /**
+     * find menus that haven't group id
+     *
+     * @param columns array of columns
+     * @return set of {@link OeMenu}
+     * @throws OeRpcException
+     * @see OeCriteriaBuilder
+     */
+    public Set<OeMenu> findByNoGroup(String... columns) throws OeRpcException {
         OeCriteriaBuilder cb = new OeCriteriaBuilder();
         cb.column(_GROUPS_ID).eq(null);
-        return (new HashSet<OeMenu>(super.find(cb)));
+        return (new HashSet<OeMenu>(super.find(cb, columns)));
     }
 
-    public Set<OeMenu> findByNoGroupParentId(Long parentId) throws OeRpcException {
+    /**
+     * find menus that haven't no group id and have parent id
+     * <p>
+     * if <code>parentId</code> is null this method uses parent_id else uses parent_id.id
+     *
+     * @param parentId menu parent id
+     * @param columns  array of columns
+     * @return set of {@link OeMenu}
+     * @throws OeRpcException
+     * @see OeCriteriaBuilder
+     * @see #find(OeCriteriaBuilder, String...)
+     */
+    public Set<OeMenu> findByNoGroupParentId(Long parentId, String... columns) throws OeRpcException {
         OeCriteriaBuilder cb = new OeCriteriaBuilder();
         cb.column(_GROUPS_ID).eq(null)
                 .andColumn((parentId != null) ? _PARENT_ID_ID : _PARENT_ID)
                 .eq(parentId);
-        return (new HashSet<OeMenu>(super.find(cb)));
+        return (new HashSet<OeMenu>(super.find(cb, columns)));
     }
 
+    /**
+     * find menus by group(s) id
+     *
+     * @param groupId group id(s)
+     * @return set of {@link OeMenu}
+     * @throws OeRpcException
+     * @see OeCriteriaBuilder
+     * @see #find(OeCriteriaBuilder, String...)
+     */
     public Set<OeMenu> findByGroupId(Long... groupId) throws OeRpcException {
         OeCriteriaBuilder cb = new OeCriteriaBuilder();
         cb.column(_GROUPS_ID_ID).in(groupId);
         return (new HashSet<OeMenu>(super.find(cb)));
     }
 
+    /**
+     * find menus by group id(s) and parent id
+     *
+     * @param parentId menu parent id
+     * @param groupId  group id
+     * @return set of menus
+     * @throws OeRpcException
+     * @see OeCriteriaBuilder
+     * @see #find(OeCriteriaBuilder, String...)
+     */
     public Set<OeMenu> findByGroupIdParentId(Long parentId, Long... groupId) throws OeRpcException {
         OeCriteriaBuilder cb = new OeCriteriaBuilder();
         cb.column(_GROUPS_ID_ID).in(groupId)
